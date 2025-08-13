719db88dc3c28a16f67d95654c9588098c3e1b93
nyxi-dev <whimsynyxi@gmail.com>
2025-08-12 20:47:07 -0700

    Update stackbit.config.ts

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -2,6 +2,11 @@
 import { defineStackbitConfig, SiteMapEntry } from "@stackbit/types";
 
 export default defineStackbitConfig({
+  "stackbitVersion": "~0.6.0",
+    "nodeVersion": "18",
+    "ssgName": "custom",
+    "contentSources": [],
+    "postInstallCommand": "npm i --no-save @stackbit/types"
   // ...
   contentSources: [
     new GitContentSource({
